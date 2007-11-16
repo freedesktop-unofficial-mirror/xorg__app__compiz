0273555e56e98bb0bedc7f3f4ad5a199edc56787
David Reveman <davidr@novell.com>
2007-11-16 12:14:26 -0500

    Start splitting up core.h into seperate files.

---
+++ include/compiz/container.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2007 Novell, Inc.
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the name of
+ * Novell, Inc. not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior permission.
+ * Novell, Inc. makes no representations about the suitability of this
+ * software for any purpose. It is provided "as is" without express or
+ * implied warranty.
+ *
+ * NOVELL, INC. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+ * NO EVENT SHALL NOVELL, INC. BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Author: David Reveman <davidr@novell.com>
+ */
+
+#ifndef _COMPIZ_CONTAINER_H
+#define _COMPIZ_CONTAINER_H
+
+#define COMPIZ_CONTAINER_VERSION 20071116
+
+COMPIZ_BEGIN_DECLS
+
+typedef struct _CompContainer {
+    CompObject base;
+
+    CompObjectVTableVec object;
+
+    ForEachChildObjectProc forEachChildObject;
+} CompContainer;
+
+#define GET_CONTAINER(object) ((CompContainer *) (object))
+#define CONTAINER(object) CompContainer *c = GET_CONTAINER (object)
+
+#define CONTAINER_TYPE_NAME "container"
+
+CompObjectType *
+getContainerObjectType (void);
+
+COMPIZ_END_DECLS
+
+#endif
---
+++ include/compiz/privates.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright © 2007 Novell, Inc.
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the name of
+ * Novell, Inc. not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior permission.
+ * Novell, Inc. makes no representations about the suitability of this
+ * software for any purpose. It is provided "as is" without express or
+ * implied warranty.
+ *
+ * NOVELL, INC. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+ * NO EVENT SHALL NOVELL, INC. BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Author: David Reveman <davidr@novell.com>
+ */
+
+#ifndef _COMPIZ_PRIVATES_H
+#define _COMPIZ_PRIVATES_H
+
+COMPIZ_BEGIN_DECLS
+
+typedef union _CompPrivate {
+    void	  *ptr;
+    long	  val;
+    unsigned long uval;
+    void	  *(*fptr) (void);
+} CompPrivate;
+
+typedef CompBool (*PrivatesCallBackProc) (CompPrivate **pPrivates,
+					  void	      *data);
+
+typedef CompBool (*ForEachPrivatesProc) (PrivatesCallBackProc proc,
+					 void		      *data,
+					 void		      *closure);
+
+
+int
+allocatePrivateIndex (int		  *len,
+		      int		  **sizes,
+		      int		  *totalSize,
+		      int		  size,
+		      ForEachPrivatesProc forEachPrivates,
+		      void		  *closure);
+
+void
+freePrivateIndex (int		      *len,
+		  int		      **sizes,
+		  int		      *totalSize,
+		  ForEachPrivatesProc forEachPrivates,
+		  void		      *closure,
+		  int		      index);
+
+CompPrivate *
+allocatePrivates (int len,
+		  int *sizes,
+		  int totalSize);
+
+COMPIZ_END_DECLS
+
+#endif
